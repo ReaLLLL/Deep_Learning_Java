352ae47af75600bed1903364e48d4aeab057f98c
Apple <songyu@u51.com>
2018-06-04 16:52:54 +0800

    lock 面试题

--- src/main/java/com/maowei/learning/lock/LockTest.java
+++ src/main/java/com/maowei/learning/lock/LockTest.java
@@ -1,8 +1,18 @@
 package com.maowei.learning.lock;
 
-import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.*;
 
 public class LockTest {
+    private  final Lock lock1;
+    private  final ReadWriteLock lock2;
+    private  final Lock lock3;
+
+    public LockTest(){
+        this.lock1 = new ReentrantLock();
+        this.lock2 = new ReentrantReadWriteLock();
+        this.lock3 = new ReentrantLock();
+    }
+
     public static void main(String[] args){
         Lock lock = new MyLock();
         for(int i = 0; i < 10; i++){
@@ -10,5 +20,39 @@ public class LockTest {
             //t.setDaemon(true);
             t.start();
         }
+    }
+
+    public void A()
+    {
+        this.lock1.lock();
+
+        this.lock2.writeLock().lock();
+
+        System.out.print("This is A method!");
+
+        this.lock2.writeLock().unlock();
+
+        this.lock1.unlock();
+    }
+
+    public void B(){
+        this.lock2.readLock().lock();
+
+        System.out.print("This is B method!");
+
+        this.lock2.readLock().unlock();
+    }
+
+    public void C()
+    {
+        this.lock3.lock();
+
+        this.lock2.writeLock().lock();
+
+        System.out.print("This is C method!");
+
+        this.lock2.writeLock().unlock();
+
+        this.lock3.unlock();
     }
 }
